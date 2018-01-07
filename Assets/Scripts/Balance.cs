e720967a211006fc3cc6b0e1bfdec8cfb91319ef
TheLetterSix <lmaclean234@gmail.com>
2018-01-07 07:32:11 -1000

    Basic physics of the field

---
+++ Assets/Scripts/Balance.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balance : MonoBehaviour {
+    public HingeJoint hinge;
+    // Use this for initialization
+
+    public float min = -10, max = 10;
+    public float dampeningForce = 2, dampeningVelocity = 10;
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        JointLimits limits = hinge.limits;
+        limits.min = min;
+        limits.bounciness = 0;
+        limits.max = max;
+
+        hinge.limits = limits;
+        hinge.useLimits = true;
+        MoveToZero();
+    }
+
+    void MoveToZero()
+    {
+        float angle =  hinge.angle;
+        JointMotor motor = hinge.motor;
+        float approachSpeed = 1;
+        if ((angle < 1 && angle > 0) || (angle > -1 && angle < 0)) approachSpeed = 0.1f;
+
+        if (angle > -0.01)
+        {
+            motor.force = dampeningForce;
+            motor.targetVelocity = -dampeningVelocity * approachSpeed;
+
+        } else if (angle < -0.01)
+        {
+            motor.force = dampeningForce;
+            motor.targetVelocity = dampeningVelocity * approachSpeed;
+        }
+        hinge.motor = motor;
+    }
+}
---
+++ Assets/Scripts/Drive.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+    public GameObject backLeft, backRight, fronLeft, frontRight;
+
+    JointMotor mbackLeft, mbackRight, mfronLeft, mfrontRight;
+    //  public HingeJoint joinBackLeft,
+
+    public float speed = 255;
+
+
+    void Start () {
+        backLeft.GetComponent<HingeJoint>().useMotor = true;
+        backRight.GetComponent<HingeJoint>().useMotor = true;
+        fronLeft.GetComponent<HingeJoint>().useMotor = true;
+        frontRight.GetComponent<HingeJoint>().useMotor = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float w = Input.GetAxis("Vertical");
+        float a = Input.GetAxis("Horizontal");
+
+        TankDrive(w+a,w-a);
+    }
+
+    void TankDrive(float left, float right)
+    {
+        if (left > 1) left = 1;
+        if (left < -1) left = -1;
+        if (right > 1) right = 1;
+        if (right < -1) right = -1;
+
+        if (right != 0)
+        {
+            mbackRight = backRight.GetComponent<HingeJoint>().motor;
+            mfrontRight = frontRight.GetComponent<HingeJoint>().motor;
+
+            mbackRight.force = 10000;
+            mbackRight.targetVelocity = right * speed;
+            mfrontRight.force = 10000;
+            mfrontRight.targetVelocity = right * speed;
+
+            backRight.GetComponent<HingeJoint>().motor = mbackRight;
+            frontRight.GetComponent<HingeJoint>().motor = mfrontRight;
+
+        }
+        if (left != 0)
+        {
+            mfronLeft = fronLeft.GetComponent<HingeJoint>().motor;
+            mbackLeft = backLeft.GetComponent<HingeJoint>().motor;
+
+            mfronLeft.force = 10000;
+            mfronLeft.targetVelocity = left * speed;
+            mbackLeft.force = 10000;
+            mbackLeft.targetVelocity = left * speed;
+
+            fronLeft.GetComponent<HingeJoint>().motor = mfronLeft;
+            backLeft.GetComponent<HingeJoint>().motor = mbackLeft;
+
+        }
+    }
+}
